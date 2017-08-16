63e41d0560984dfcdc313634ebe813dc00dd7ac9
d.ilyukhin <d.ilyukhin@distillery.com>
2017-08-16 16:57:47 +0300

    Initial generate songs

---
+++ generate-songs.js
@@ -0,0 +1,99 @@
+const fs = require('fs');
+const path = require('path');
+const uuidv4 = require('uuid/v4');
+
+const GENRES = [
+  'Cat Meowing',
+  'Roaring',
+  'Whispering',
+  'Total Silence',
+  'Snore',
+  'Noise'
+];
+const FILE_PATH = path.join(__dirname, '/src/mocks/tracks.json');
+const AMOUNT = 204;
+
+generate();
+
+function generate() {
+  fs.writeFile(FILE_PATH, generateSongsJSON(AMOUNT), err => {
+    if(err) {
+      throw err;
+    }
+
+    console.log('The file has been saved!');
+  });
+}
+
+
+function generateSongsJSON(amount) {
+  if (Number.isNaN(amount) || amount < 1) {
+    throw new Error('Invalid amount');
+  }
+
+  let songs = '[]';
+  try {
+    songs = JSON.stringify(generateSongs(amount));
+  }
+  catch(e) {
+    throw e;
+  }
+  return songs;
+}
+
+
+function generateSongs(amount) {
+  if (Number.isNaN(amount) || amount < 1) {
+    throw new Error('Invalid amount');
+  }
+
+  const result = [];
+  for (let i = 0; i < amount; i++) {
+    const song = {
+      "id": generateSongId(),
+      "performer": generatePerfomerName(),
+      "name": generateSongName(),
+      "genre": generateGenre(),
+      "year": generateYear(),
+      "duration": generateDuration()
+    };
+    console.log('song: ', song);
+    result.push(song);
+  }
+
+  return result;
+}
+
+function generateSongId() {
+  return uuidv4();
+}
+
+function generatePerfomerName() {
+  return `Unknown Artist ${getRandomNumberInInterval(1, 20)}`
+}
+
+function generateSongName() {
+  return `Song ${getRandomNumberInInterval(1, 20)}`
+}
+
+function generateGenre() {
+  return GENRES[getRandomNumberInInterval(0, GENRES.length)];
+}
+
+function generateYear() {
+  return getRandomNumberInInterval(1925, 2017);
+}
+
+function generateDuration() {
+  return getRandomNumberInInterval(15 * 1000, 400 * 1000);
+}
+
+
+function getRandomNumberInInterval(min, max) {
+  if (min >= max || Number.isNaN(min) || Number.isNaN(max)) {
+    throw new Error(`Invalid interval:  ${min} ${max}`);
+  }
+  return Math.round(Math.random() * (max - min) + min);
+}
+
+
