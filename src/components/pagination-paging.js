a0ff3ddf7f200af0fc138254ff527f4c37b5cf25
d.ilyukhin <d.ilyukhin@distillery.com>
2017-08-17 14:10:01 +0300

    Pagination basics

--- src/components/pagination-paging.js
+++ src/components/pagination-paging.js
@@ -4,24 +4,53 @@ class PaginationPaging extends Component {
 
   constructor(props) {
     super(props);
-    // this.handleSorting = this.handleSorting.bind(this);
+    this.handlePageClick = this.handlePageClick.bind(this);
   }
 
-  // handleSorting(fieldData) {
-  //   this.props.handleSorting(fieldData);
-  // }
-
+  handlePageClick(e) {
+    console.log(e.target.value); // TODO:
+    this.props.handlePageClick(e.target.value);
+  }
 
   render() {
     const page = this.props.page;
     const totalPages = this.props.totalPages;
     if (!page || !totalPages) return null;
     return (
-      <div>
-        page: {page}. Total pages: {totalPages}
+      <div className='paging'>
+        <div>{'<<'}</div>
+        <div>{'<'}</div>
+          {PaginationPaging.generatePages(totalPages, page)}
+        <div>{'>'}</div>
+        <div>{'>>'}</div>
       </div>
     );
   }
+
+  static generatePages(total, page) {
+    if (!total || !page) throw new Error('Invalid arguments!');
+    const AROUND_PAGES = 4; // should be even
+    const result = [];
+
+    let from = (page - AROUND_PAGES < 1) ? 1 : (page - AROUND_PAGES < 1);
+    let to = (page + AROUND_PAGES > total) ? total : (page + AROUND_PAGES);
+    console.log('from: ', from, ' to: ', to);
+    for (let i = from; i <= to; i++) {
+      const button = PaginationPaging.generatePageButton(i);
+      result.push(button);
+    }
+    return result;
+
+  }
+
+  static generatePageButton(number) {
+    if (!number) throw new Error('Invalid arguments!');
+    return (
+      <div onClick={this.handlePageClick} className='paging__button' key={number}>{number}</div>
+
+    )
+  }
+
 }
 
 export default PaginationPaging;
